21394dc54e82a69e97ff3909d5f737e6d16c9258
Rob Larsen <rob@htmlcssjavascript.com>
2016-03-07 00:56:17 -0500

    another experiment

---
+++ experiments/react-webpack/src/components/Main.js
@@ -0,0 +1,18 @@
+require('bootstrap-loader');
+
+require('styles/Styles.css');
+
+import React from 'react';
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div/>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
